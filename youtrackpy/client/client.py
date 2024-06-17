3fc2984b2f880ef06894c16d0de029afdcd156a5
Álvaro Soto <asotocunillera@gmail.com>
2024-06-17 19:34:02 +0200

    remove unnecessary code in project model

--- youtrackpy/client/client.py
+++ youtrackpy/client/client.py
@@ -1,4 +1,4 @@
-from typing import Any, Literal, TypedDict 
+from typing import Any, Literal, TypedDict
 
 import requests
 from requests.structures import CaseInsensitiveDict
--- youtrackpy/resources/project.py
+++ youtrackpy/resources/project.py
@@ -36,8 +36,6 @@ class Project:
     shortName: str
     name: str | None = None
 
-    _info: ProjectEntity | None = None
-
     def __repr__(self) -> str:
         if self.name is None:
             return f"Project({self._client!r},shortName={self.shortName!r})"
@@ -47,9 +45,6 @@ class Project:
 
     @property
     def info(self) -> ProjectEntity:
-
-        if self._info is not None:
-            return self._info
 
         project = self._client.get(
             endpoint=f"{PROJECT_ENDPOINT}/{self.shortName}",
@@ -63,31 +58,25 @@ class Project:
 
         project = project["body"]
 
-        self._info = self.__update_info(project)
-
-        return self._info
-
-    def __update_info(self, project_info: dict[str, Any]) -> ProjectEntity:
-        # TODO add rest of values
         return ProjectEntity(
             shortName=self.shortName,
-            name=project_info.get("name"),
-            id=project_info.get("id"),
-            archived=project_info.get("archived"),
-            createdBy=project_info.get("createdBy"),
-            customFields=project_info.get("customFields"),
-            description=project_info.get("description"),
-            fromEmail=project_info.get("fromEmail"),
-            iconUrl=project_info.get("iconUrl"),
-            issues=self._save_issues(project_info.get("issues")),
-            leader=project_info.get("leader"),
-            replyToEmail=project_info.get("replyToEmail"),
-            startingNumber=project_info.get("startingNumber"),
-            team=project_info.get("team"),
-            template=project_info.get("template"),
+            name=project.get("name"),
+            id=project.get("id"),
+            archived=project.get("archived"),
+            createdBy=project.get("createdBy"),
+            customFields=project.get("customFields"),
+            description=project.get("description"),
+            fromEmail=project.get("fromEmail"),
+            iconUrl=project.get("iconUrl"),
+            issues=self._format_issues(project.get("issues")),
+            leader=project.get("leader"),
+            replyToEmail=project.get("replyToEmail"),
+            startingNumber=project.get("startingNumber"),
+            team=project.get("team"),
+            template=project.get("template"),
         )
 
-    def _save_issues(self, issues: list[dict[str, Any]] | None) -> list[IssueEntity]:
+    def _format_issues(self, issues: list[dict[str, Any]] | None) -> list[IssueEntity]:
         if issues is None:
             return []
 
